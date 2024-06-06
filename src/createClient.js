7c6dbbbc014fe265153b24e3f8a852203164b8da
veerabala <imveerabala@gmail.com>
2024-06-06 12:28:25 +0530

    second commit

--- src/createClient.js
+++ src/createClient.js
@@ -5,5 +5,5 @@ const supabaseUrl = import.meta.env.VITE_API_URL
 // const supabaseKey = process.env.SUPABASE_KEY
 const supabaseKey = import.meta.env.VITE_API_KEY
 
-// 1234poiuQWER!
+
 export const supabase = createClient(supabaseUrl, supabaseKey)
